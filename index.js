102a81e1e3a3b9f30d576eed4e29b97072991663
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-15 23:42:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,55 @@
+function add (a,b)
+
+{
+  
+  return a + b
+  
+}
+
+function substract(a,b)
+
+
+{
+  
+  return a - b
+  
+}
+
+
+function multiply (a,b)
+
+
+{
+  
+  return a * b
+  
+}
+
+
+
+function inc (n)
+
+{
+  
+  return n += 1
+  
+}
+
+
+function dec (n)
+
+{
+  
+  return n -= 1
+  
+}
+
+
+function makeInt (n)
+
+
+{
+  
+  return parseInt(n)
+  
+}
